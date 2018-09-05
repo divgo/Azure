c09825141c98723252f51677a34e9f95fc8964eb
Sage McEnery <smcenery@ashleyfurniture.com>
2018-09-05 14:47:42 -0400

    Deploy a ZipFile to an App Service

---
+++ AppServices/Deploy Zip To AppService.ps1
@@ -0,0 +1,17 @@
+﻿Connect-AzureRMAccount
+Select-AzureRmSubscription -Subscription "ECOMM PRODUCTION"
+
+$ResourceGroup = "licenseesalesdata-rg"
+$SiteName = "licenseesalesdata-web"
+
+$Profile = [xml](Get-AzureRMWebAppPublishingProfile -ResourceGroupName $ResourceGroup -Name $SiteName -Format WebDeploy)
+$UserNode = $Profile.FirstChild.ChildNodes[0]
+
+$ApiURL = "https://" + $usernode.publishUrl + "/api/zipdeploy"
+$Creds = [CONVERT]::ToBase64String( [Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $userNode.userName, $userNode.userPWD)) )
+$Headers = @{Authorization=("Basic {0}" -f $Creds)}
+
+write-Host "Publishing Zip to " + $usernode.publishUrl
+
+Invoke-RestMethod -Uri $APIUrl -ContentType "multipart/form-data" -Headers $Headers -InFile "C:\Users\SMcEnery\Web.zip" -Method POST
+
